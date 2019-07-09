a2d3d16d26fe596904234f8fe05e1fbd289e17aa
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2019-07-09 09:53:07 +0200

    feat(organism/mediaGallery): first commit component creation

---
+++ components/organism/mediaGallery/README.md
@@ -0,0 +1,23 @@
+# OrganismMediaGallery
+
+> Description
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @s-ui/react-organism-media-gallery --save
+```
+
+## Usage
+
+### Basic usage
+```js
+import OrganismMediaGallery from '@s-ui/react-organism-media-gallery'
+
+return (<OrganismMediaGallery />)
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/organism/mediaGallery/src/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+// import PropTypes from 'prop-types'
+
+class OrganismMediaGallery extends Component {
+  render() {
+    return (
+      <div className="react-OrganismMediaGallery">
+        <h1>OrganismMediaGallery</h1>
+      </div>
+    )
+  }
+}
+
+OrganismMediaGallery.displayName = 'OrganismMediaGallery'
+
+// Remove these comments if you need
+// OrganismMediaGallery.contextTypes = {i18n: PropTypes.object}
+// OrganismMediaGallery.propTypes = {}
+// OrganismMediaGallery.defaultProps = {}
+
+export default OrganismMediaGallery
