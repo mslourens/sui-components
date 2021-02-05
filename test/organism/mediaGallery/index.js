e026ef33dddd97442ceaa02550fbd1e801b79027
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2021-02-05 19:57:14 +0100

    test(Root): add tests

--- test/organism/mediaGallery/index.js
+++ test/organism/mediaGallery/index.js
@@ -3,20 +3,106 @@
  * */
 
 /* eslint react/jsx-no-undef:0 */
+/* eslint no-undef:0 */
 
 // import React from 'react'
 // import {render} from '@testing-library/react'
-
-import chai, {expect} from 'chai'
+import ReactDOM from 'react-dom'
+import chai from 'chai'
 import chaiDOM from 'chai-dom'
 
 chai.use(chaiDOM)
 
-describe('organism/mediaGallery', () => {
-  it('Render', () => {
-    // Example TO BE DELETED!!!!
-    // const {getByRole} = render(<AtomButton>HOLA</AtomButton>)
-    // expect(getByRole('button')).to.have.text('HOLA')
-    expect(true).to.be.eql(false)
+describe('Organism Media Gallery', () => {
+  const Component = OrganismMediaGallery
+  it('should render without crashing', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should show media buttons when different media types given', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should hide media buttons when just one media type given', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should open fullwidth when user press button', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should show thumbnails', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should change image when click on thumbnail', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should show a counter when swipping images', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
   })
 })
