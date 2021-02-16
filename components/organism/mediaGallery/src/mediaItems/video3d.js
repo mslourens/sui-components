0ccb8357a27bb27919cf631271ab91e9d48dbc6c
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2021-02-16 12:39:56 +0100

    feat(organism/mediaGallery): add title prop to video3d

--- components/organism/mediaGallery/src/mediaItems/video3d.js
+++ components/organism/mediaGallery/src/mediaItems/video3d.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types'
 
-function Video3d(props) {
-  const {src, ...videoProps} = props
-  return <iframe src={src} {...videoProps} />
+function Video3d({src, title, ...videoProps}) {
+  return <iframe src={src} title={title} {...videoProps} />
 }
 
 Video3d.propTypes = {
   /**
    * Video source
    */
-  src: PropTypes.string.isRequired
+  src: PropTypes.string.isRequired,
+  /**
+   * Video title
+   */
+  title: PropTypes.string
 }
 
 export default Video3d
