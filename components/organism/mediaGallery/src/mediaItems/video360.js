8ea9b3d94505921e5e7ddc6ee9c71362147eeac4
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2021-02-16 12:40:49 +0100

    feat(organism/mediaGallery): add title prop to video 360

--- components/organism/mediaGallery/src/mediaItems/video360.js
+++ components/organism/mediaGallery/src/mediaItems/video360.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types'
 
-function Video360(props) {
-  const {src, ...videoProps} = props
-  return <iframe src={src} {...videoProps} />
+function Video360({src, title, ...videoProps}) {
+  return <iframe src={src} title={title} {...videoProps} />
 }
 
 Video360.propTypes = {
   /**
    * Video source
    */
-  src: PropTypes.string.isRequired
+  src: PropTypes.string.isRequired,
+  /**
+   * Video title
+   */
+  title: PropTypes.string
 }
 
 export default Video360
